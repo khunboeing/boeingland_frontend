c25ce0b6450d125cc8762e3ffa217621902f9243
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-05-07 16:59:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="underline font-bold">boeing land</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
